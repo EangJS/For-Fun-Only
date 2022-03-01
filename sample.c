d617e4a00b99f1ca02470bafbb6364ded3b72320
EangJS <eugeneang654871@gmail.com>
2022-03-01 19:34:41 +0800

    sample.c

---
+++ sample.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+int main(int argc, const char *argv[]) {
+    char *s = malloc(100);
+    strcpy(s, "Hello world!");
+    printf("string is: %s\n", s);
+    s[101] = 'a';
+    return 0;
+}
